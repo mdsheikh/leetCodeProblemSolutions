9c64714bd34f32dd4b1ca96f85f44c818245ee01
Md <mdsheikh56@yahoo.com>
2020-08-20 23:30:45 -0400

    Balance Binary Tree

---
+++ balanceBinaryTree.java
@@ -0,0 +1,39 @@
+import org.graalvm.compiler.core.phases.EconomyHighTier;
+
+// definition of a binary tree node
+// public class TreeNode {
+//     int val;
+//     TreeNode left;
+//     TreeNode right;
+
+//     TreeNode() {
+//     }
+
+//     TreeNode(int val) {
+//         this.val = val;
+//     }
+
+//     TreeNode(int val, TreeNode left, TreeNode right) {
+//         this.val = val;
+//         this.left = left;
+//         this.right = right;
+//     }
+// }
+
+public class balanceBinaryTree {
+    public boolean isBalanced(TreeNode root) {
+        return height(root) >= 0;
+    }
+
+    private int height(TreeNode root) {
+        if (root == null)
+            return 0;
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        if (leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+}
