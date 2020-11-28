facf0d008cde97bef8657fc1b618f7ec80fcec99
Md Sheikh <mdsheikh56@yahoo.com>
2020-11-28 00:21:01 -0500

    Fixed twoSum problem

--- twoSum.java
+++ twoSum.java
@@ -1,10 +1,21 @@
+
 public class twoSum{
+
     public static int[] two_Sum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
-                if (nums[j] == target - nums[i]) {
-                    return new int[] { i, j };
+
+                int target_sum = nums[i] + nums[j];
+
+                if(target_sum == target){
+                    int[] result = new int[2];
+                    result[0] = i;
+                    result[1] = j;
+
+                    return result;
+
                 }
+               
             }
         }
         throw new IllegalArgumentException("No two sum solution");
@@ -12,10 +23,10 @@ public class twoSum{
 
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7,8,9,10};
-        int target = 18;
+        int target = 3;
         int result[] = two_Sum(arr, target);
         for(int i : result){
-             System.out.println(i);
+             System.out.print(i + " ");
          }
     }
    
